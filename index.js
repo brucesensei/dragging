03739ec0f006e5d796723b21e36cde28d5a63f1f
nolan bruce <nolan_76@msn.com>
2022-11-18 18:46:01 +0900

    changed the name a some pages and added the forms page as home

--- index.js
+++ index.js
@@ -7,15 +7,37 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+var scram = {} 
+
+
 app.get('/', function(req, res) { 
-    res.render('index');
+    res.render('home');
 });
 
-app.get('/quiz-form', function(req, res) {
-    res.render('quiz-form');
+app.post('/', function(req, res) {
+    var scram = {
+        title: req.body.formTitle,
+        example: req.body.formExample,
+        motherTounge: req.body.formTranslate,
+        sentenceList: [
+          {
+            beginning: req.body.formBeginning,
+            middle: req.body.formMiddle,
+            end: req.body.formEnd,
+        },
+          ],
+        };
 });
+
+app.get('/quiz', function(req, res) {
+    res.render('quiz');
+});
+
 
 
 app.listen(3000, function() {
     console.log('server running on port 3000');
 });
+
+// if the form box is not checekd the return value is undefined
+// if it is checked the name 'includeTranlation' is returned.
