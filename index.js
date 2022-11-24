52d0259eccda5505366ab8fd6538de4f6180316a
nolan bruce <nolan_76@msn.com>
2022-11-24 18:41:15 +0900

    putting the project to bed until I find some more suitable tools to help me achieve my goal.

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { json } = require('body-parser');
 const app = express();
+const programs = require(__dirname + '/programs.js');
+
+
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-var scram = {} 
-
+var scram = {}; 
+var translation =[];
 
 app.get('/', function(req, res) { 
     res.render('home');
 });
 
 app.post('/', function(req, res) {
-    scram = req.body;
+    var userInput = programs.parseDict(req.body);
+    scram = userInput[0];
+    translation = userInput[1];
     console.log(scram);
+    console.log(translation);
     
 });
 
